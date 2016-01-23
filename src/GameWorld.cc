ed32cba67d066313fcdb70f4000d8c1c9d93e4eb
Jonny <jonny234x3@ln-mowifi-02-10.1.31.245.net.bton.ac.uk>
2016-01-23 13:38:26 +0000

    Changed the rotation of the word 90% clockwise. NOTE: array length needs to be equal X by Y

--- src/GameWorld.cc
+++ src/GameWorld.cc
@@ -9,20 +9,24 @@ GameWorld::GameWorld (ApplicationMode mode) : asset_manager (make_shared<GameAss
 
 int X,Y;
 int Z = -10;
-int planeX = 5;
-int planeY = 17;
+int planeX = 12;
+int planeY = 12;
 
 int plane[planeX][planeY] = {
-{1, 0, 0, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1},
-{1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0},
-{1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0},
-{1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0},
-{1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 0, 1, 0}
-};
 
-///
-///spawning multiple cubes
-// asset_manager->AddAsset(std::make_shared<CubeAsset>(1.0f,2.0f,3.0f));
+{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+{1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1},
+{1, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0},
+{1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1},
+{1, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0},
+{1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 0},
+{1, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 1},
+{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+};
 
 ///
 ///For loop to read positions in the array.
@@ -31,8 +35,9 @@ int plane[planeX][planeY] = {
 
  for( X=0; X<planeX; X++){
    for (Y=0; Y<planeY; Y++){
-    if( plane[X][Y] == 1){  
-      asset_manager->AddAsset(std::make_shared<CubeAsset>(X, Y, Z));
+    if( plane[Y][X] == 1){  
+      asset_manager->AddAsset(std::make_shared<CubeAsset>(X, -Y, Z));
+      //spawning a cube
     }    
    }
  }
