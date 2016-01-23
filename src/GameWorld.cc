86c4baec6e829e1d22b9f0aa1bcc1df412c79e13
Jonny <jonny234x3@ln-mowifi-02-10.1.31.245.net.bton.ac.uk>
2016-01-23 13:22:24 +0000

    using a 2D array to spell a word with the cube assets. NOTE: need to fix direction as the word is rotated 90% anticlockwise.

--- src/GameWorld.cc
+++ src/GameWorld.cc
@@ -1,14 +1,42 @@
 #include "GameWorld.h"
 using namespace std;
 GameWorld::GameWorld (ApplicationMode mode) : asset_manager (make_shared<GameAssetManager>(mode)){
+
 ///
-///Cube spawning
-///using asset_manager to add multiple instances of the cube assest.]
+///Cube spawning with Array
+///Using a 2D array to spawn multiple voxels, to spell a word
+///
+
+int X,Y;
+int Z = -10;
+int planeX = 5;
+int planeY = 17;
+
+int plane[planeX][planeY] = {
+{1, 0, 0, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1},
+{1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0},
+{1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0},
+{1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0},
+{1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 0, 1, 0}
+};
+
+///
 ///spawning multiple cubes
+// asset_manager->AddAsset(std::make_shared<CubeAsset>(1.0f,2.0f,3.0f));
+
 ///
-  
-  asset_manager->AddAsset(std::make_shared<CubeAsset>(-1.0f,-2.0f,-3.0f));
-  asset_manager->AddAsset(std::make_shared<CubeAsset>(1.0f,2.0f,3.0f));
+///For loop to read positions in the array.
+///using asset_manager to add the instance of a cube.
+///
+
+ for( X=0; X<planeX; X++){
+   for (Y=0; Y<planeY; Y++){
+    if( plane[X][Y] == 1){  
+      asset_manager->AddAsset(std::make_shared<CubeAsset>(X, Y, Z));
+    }    
+   }
+ }
+
 }
 
 void GameWorld::Draw() {
