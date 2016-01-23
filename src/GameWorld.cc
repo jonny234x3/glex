18d18878985eccb8f576bbb93ed976f2ca4b37bb
Jonny <jonny234x3@ln-mowifi-02-10.1.31.245.net.bton.ac.uk>
2016-01-23 12:57:04 +0000

    fixing cpp check error

--- src/GameWorld.cc
+++ src/GameWorld.cc
@@ -1,12 +1,12 @@
 #include "GameWorld.h"
-
-GameWorld::GameWorld (ApplicationMode mode) {
+using namespace std;
+GameWorld::GameWorld (ApplicationMode mode) : asset_manager (make_shared<GameAssetManager>(mode)){
 ///
 ///Cube spawning
 ///using asset_manager to add multiple instances of the cube assest.]
 ///spawning multiple cubes
 ///
-  asset_manager = std::make_shared<GameAssetManager>(mode);
+  
   asset_manager->AddAsset(std::make_shared<CubeAsset>(-1.0f,-2.0f,-3.0f));
   asset_manager->AddAsset(std::make_shared<CubeAsset>(1.0f,2.0f,3.0f));
 }
