7ba516f654a10ed2d58128bc7f0963939edf6c69
Jonny <jonny234x3@localhost.localdomain>
2016-01-26 15:37:45 +0000

    Changed the cube array to be horizontal plane. Next is to recreate this as a floorasse for the plane

--- src/GameWorld.cc
+++ src/GameWorld.cc
@@ -8,13 +8,13 @@ GameWorld::GameWorld (ApplicationMode mode) : asset_manager (make_shared<GameAss
 ///
 
 int X,Y;
-int Z = -10;
+int Z = 1;
 int planeX = 12;
 int planeY = 12;
 
 int plane[planeX][planeY] = {
 
-{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
 {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
 {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
 {1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1},
@@ -25,7 +25,7 @@ int plane[planeX][planeY] = {
 {1, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 1},
 {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
 {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}
 };
 
 ///
@@ -39,14 +39,17 @@ asset_manager->AddAsset(std::make_shared<PyramidAsset>(3.0f, 2.0f, -2.0f));
 asset_manager->AddAsset(std::make_shared<PyramidAsset>(0.0f, 2.0f, -6.0f));
 
 ///
-///For loop to read positions in the array.
-///using asset_manager to add the instance of a cube.
+///Spawning in Ground
+///Ground is spawned with;
+///X offset equal to the half of the x plane so the positions are roughly center
+///Y is a constant as the ground is one flat plane
+///Z is determined by the Y value
 ///
 
  for( X=0; X<planeX; X++){
    for (Y=0; Y<planeY; Y++){
     if( plane[Y][X] == 1){  
-      asset_manager->AddAsset(std::make_shared<CubeAsset>(X, -Y, Z));
+      asset_manager->AddAsset(std::make_shared<CubeAsset>(X-(planeX/2), -5.0f, (Z*Y)-10));
       //spawning a cube
     }    
    }
