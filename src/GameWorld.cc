b69e2ad300f7ecab9674c48a267bb885d065eb04
Jonny234x3 <jonathonohalloran@gmail.com>
2016-05-04 22:50:49 +0100

    Changed gameworld.cc to work for the additional camera. Minor doxygen updates. Note that higher assets must be placed before the floor asset else their dissapear behind it. This may be due to the way they are drawn or called.

--- src/GameWorld.cc
+++ src/GameWorld.cc
@@ -4,17 +4,17 @@
 using namespace std;
 GameWorld::GameWorld (ApplicationMode mode) : asset_manager (make_shared<GameAssetManager>(mode)){
 
-///
+//////////////////////////////////////////////////////////////////////////////////
 /// Cube spawning with Array
 /// Using a 2D array to spawn multiple voxels
 /// 1 - Floor piece
 /// 2 - Floor piece and cube
 /// 3 - Floor piece and pyramid
-///
+//////////////////////////////////////////////////////////////////////////////////
 
-int X,Y;
-int Z = -1;
-int planeX = 12;
+int X,Y; //point A,B
+int Z = 1;
+int planeX = 12;  //worldspace
 int planeY = 12;
 
 int plane[planeX][planeY] = {
@@ -32,35 +32,39 @@ int plane[planeX][planeY] = {
 {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
 };
 
-///
+//////////////////////////////////////////////////////////////////////////////////
 /// Spawning Floor
 /// floor is spawned with;
-/// X offset equal to the half of the x plane so the positions are roughly center
+/// The values in the array above act like coordinate points.
 /// Y is a constant as the floor is one flat plane
 /// Z is determined by the Y value
-///
+//////////////////////////////////////////////////////////////////////////////////
+  /// We can use the above array as coordinate points, [1][3] would be
+  /// coordinates x1 y3. In this way we can generate a 3D world using
+  /// numbers to represent objects.
+  ///////////////////////////////////////////////////////////////////
 
  for( X=0; X<planeX; X++){
    for (Y=0; Y<planeY; Y++){
-    if( plane[Y][X] == 1){  
-     asset_manager->AddAsset(make_shared<FloorAsset>((X)-(planeX/2.0f),-6.00f,(Z*Y)));
+    if( plane[Y][X] == 1){
+     asset_manager->AddAsset(make_shared<FloorAsset>((X), -1.0f, (Y*Z)));
     }    
 
-///
+//////////////////////////////////////////////////////////////////////////////////
 /// Spawning Cubes
 /// Spawns floor asset and a Cube asset
-///
+//////////////////////////////////////////////////////////////////////////////////
     else if( plane[Y][X] == 2){
-      asset_manager->AddAsset(make_shared<FloorAsset>((X)-(planeX/2.0f),-6.00f,(Z*Y));
-      asset_manager->AddAsset(make_shared<CubeAsset>((X)-(planeX/2.0f),2.00f,(Z*Y));
+      asset_manager->AddAsset(make_shared<CubeAsset>((X), 2.0f, (Y*Z)));
+      asset_manager->AddAsset(make_shared<FloorAsset>((X), -1.0f, (Y*Z)));
     }
-///
+//////////////////////////////////////////////////////////////////////////////////
 /// Spawning Pyramids
 /// Spawns floor asset and a Pyramid asset above it
-///
+//////////////////////////////////////////////////////////////////////////////////
     else if( plane[Y][X] == 3){
-      asset_manager->AddAsset(make_shared<FloorAsset>((X)-(planeX/2.0f),-6.00f,(Z*Y)));
-      asset_manager->AddAsset(make_shared<PyramidAsset>((X)-(planeX/2.0f),3.00f,(Z*Y)));
+      asset_manager->AddAsset(make_shared<PyramidAsset>((X), 0.0f, (Y*Z)));
+      asset_manager->AddAsset(make_shared<FloorAsset>((X), -1.0f, (Y*Z)));
     }
   }
  }
