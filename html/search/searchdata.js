09b76003aec8c4ea98e1e5cebcd0002839c24ac4
Jonny234x3 <jonathonohalloran@gmail.com>
2016-05-10 16:59:51 +0100

    Doxygen Updates

--- html/search/searchdata.js
+++ html/search/searchdata.js
@@ -1,39 +1,12 @@
 var indexSectionsWithContent =
 {
-  0: "acdefghilmoprstv~",
-  1: "cfgps",
-  2: "cfgmpr",
-  3: "acdfgimoprt~",
-  4: "acdepv",
-  5: "a",
-  6: "rst",
-  7: "cghlprsv",
-  8: "p"
 };
 
 var indexSectionNames =
 {
-  0: "all",
-  1: "classes",
-  2: "files",
-  3: "functions",
-  4: "variables",
-  5: "enums",
-  6: "enumvalues",
-  7: "defines",
-  8: "pages"
 };
 
 var indexSectionLabels =
 {
-  0: "All",
-  1: "Classes",
-  2: "Files",
-  3: "Functions",
-  4: "Variables",
-  5: "Enumerations",
-  6: "Enumerator",
-  7: "Macros",
-  8: "Pages"
 };
 
